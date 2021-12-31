54a953f0988cc69feb74736296f75032ba3b2330
tiendung <tiendung1001gn@gmail.com>
2021-12-31 11:12:33 +0700

    first commit

---
+++ src/main/java/edu/tiendung/hibernate/config/AppInit.java
@@ -0,0 +1,30 @@
+package edu.tiendung.hibernate.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author Ramesh Fadatare
+ */
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class <?> [] getRootConfigClasses() {
+        return new Class[] {
+            AppContext.class
+        };
+    }
+
+    @Override
+    protected Class <?> [] getServletConfigClasses() {
+        return new Class[] {
+            AppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {
+            "/"
+        };
+    }
+}
